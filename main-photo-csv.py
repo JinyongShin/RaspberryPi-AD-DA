18f8521eabacf6b87df869a25e0619d211cc6e1c
jamminj12 <72026333+jamminj12@users.noreply.github.com>
2021-06-01 11:58:36 -0400

    Update main-photo-csv.py

--- main-photo-csv.py
+++ main-photo-csv.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding:utf-8 -*-
 
 from time import sleep, strftime, time
@@ -8,8 +7,9 @@ import ADS1256
 import RPi.GPIO as GPIO
 
 
-file = open("/home/pi/data_log_test3.csv", "a")
-if os.stat("/home/pi/data_log_test3.csv").st_size == 0:
+os.chdir("/home/pi/Data_Logging_Test") # test folder for 
+file = open("/home/pi/data_log_test.csv", "a")
+if os.stat("/home/pi/data_log_test.csv").st_size == 0:
         file.write("Data #,Time,Potentiometer,Photosensor\n")
 
 try:
@@ -17,13 +17,13 @@ try:
     ADC.ADS1256_init()
 
 # when "while" used, no stoppping the recording of data, must be manually stopped
-# when "used" used, make sure to include range() with the amount of data points requested
+# when "used" used, make sure to include range() with the amount of data points requested. Currently recording only 100 data values
     #while True:
     for x in range(100):
         ADC_Value = ADC.ADS1256_GetAll()
         file.write(str(x)+","+str(datetime.datetime.now())+","+str(ADC_Value[0]*5.0/0x7fffff)+","+str(ADC_Value[1]*5.0/0x7fffff)+"\n")
         file.flush()
-        sleep(0.5) #recording data every 0.5 seconds
+        sleep(0.1) # recording data every 0.1 seconds (or in other words 10 samples a second, 10 Hz) 
         print ("current time:-",datetime.datetime.now())
         print ("Potentiometer (0 ADC)= %lf"%(ADC_Value[0]*5.0/0x7fffff))
         print ("Photosensor Voltage (1 ADC)= %lf"%(ADC_Value[1]*5.0/0x7fffff))
